ace3b1de1f2beaf52a6d2699d9ff77dabd2f2f91
bloihl <bobloihl@yahoo.com>
2024-01-08 23:44:33 -0800

    extract a header component to eventually use FlatList in CatScreen

---
+++ src/CatHeader.tsx
@@ -0,0 +1,35 @@
+import type {PropsWithChildren} from 'react';
+import {
+    Image,
+    StatusBar,
+    Text,
+    View,
+} from 'react-native';
+
+import styles from 'AppStyle.tsx';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+type CatHeaderProps = PropsWithChildren<{
+    isDarkMode: boolean;
+}>;
+
+function CatHeader({isDarkMode}: CatHeaderProps): JSX.Element {
+   const backgroundStyle = {
+     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+   };
+
+  return (
+        <View style={styles.logoView}>
+            <StatusBar
+                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+                backgroundColor={backgroundStyle.backgroundColor}
+                />
+            <Image source={require('./cat-logo.png')} />
+            <Text style={[styles.sectionTitle]}>Columbia Area Transit Tracker</Text>
+        </View>
+  );
+}
+export default CatHeader
--- src/CatScreen.tsx
+++ src/CatScreen.tsx
@@ -14,26 +14,21 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 import styles from 'AppStyle.tsx';
+import CatHeader from 'CatHeader.tsx';
 
 type CatScreenProps = PropsWithChildren<{
     isDarkMode: boolean;
 }>;
 
-function CatScreen({children, isDarkMode}: SectionProps): JSX.Element {
+function CatScreen({children, isDarkMode}: CatScreenProps): JSX.Element {
    const backgroundStyle = {
      backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
    };
 
   return (
     <SafeAreaView style={backgroundStyle}>
-        <StatusBar
-            barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-            backgroundColor={backgroundStyle.backgroundColor}
-            />
-        <View style={styles.logoView}>
-            <Image source={require('./cat-logo.png')} />
-            <Text style={[styles.sectionTitle]}>Columbia Area Transit Tracker</Text>
-        </View>
+
+        <CatHeader />
 
         <View
           style={[{
