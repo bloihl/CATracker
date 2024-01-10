54ce0c0977b58cf63c81f34dbb508be04ca9f7d7
bloihl <bobloihl@yahoo.com>
2024-01-09 21:30:19 -0800

    more tidying up imports

--- App.tsx
+++ App.tsx
@@ -1,11 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React, {useState, useContext} from 'react';
+import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
